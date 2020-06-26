d6cd22d17343670a06b8ac315169f49f66520ed0
Zak Koltun <zakkoltun@gmail.com>
2020-06-25 17:52:57 -0700

    Rock paper scissors modifications

--- lesson-2/rock-paper-scissors.js
+++ lesson-2/rock-paper-scissors.js
@@ -1,21 +1,56 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
-const WINNING_COMBINATIONS = {
-  rock: 'scissors',
-  paper: 'rock',
-  scissors: 'paper'
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
+
+const WINNING_CHOICES = {
+  rock: ['scissors', 'lizard'],
+  paper: ['rock', 'spock'],
+  scissors: ['paper', 'lizard'],
+  spock: ['scissors', 'rock'],
+  lizard: ['paper', 'spock']
 };
+
+const RESULT_RESPONSES = {
+  player: 'You win!',
+  computer: 'Computer wins!',
+  tie: "It's a tie"
+};
+
+let playerWins = 0;
+let computerWins = 0;
+
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function isWinningChoice(playerChoice, opposingPlayerChoice) {
-  return WINNING_COMBINATIONS[playerChoice] === opposingPlayerChoice;
+function pickWinner(choice, computerChoice) {
+  if (WINNING_CHOICES[choice].includes(computerChoice)) {
+    return 'player';
+  } else if (WINNING_CHOICES[computerChoice].includes(choice)) {
+    return 'computer';
+  } else {
+    return 'tie';
+  }
 }
 
-while (true) {
+function displayWinner(choice, computerChoice, winner) {
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+  prompt(RESULT_RESPONSES[winner]);
+  prompt(`You have ${playerWins} wins, computer has ${computerWins}`);
+}
+
+function grandMaster(playerWins) {
+  if (playerWins === 5) {
+    prompt('You are the Grandmaster!!');
+  } else {
+    prompt('Computer is the Grandmaster!');
+  }
+}
+
+
+while (playerWins < 5 && computerWins < 5) {
   prompt('----------------------');
+  prompt('First to 5 wins is the grand master!\n');
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
   let choice = readline.question();
 
@@ -26,25 +61,19 @@ while (true) {
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
+  let winner = pickWinner(choice, computerChoice);
 
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
-
-  if (isWinningChoice(choice, computerChoice)) {
-    prompt('You win!');
-  } else if (isWinningChoice(computerChoice, choice)) {
-    prompt('Computer wins!');
-  } else {
-    prompt("It's a tie");
+  switch (winner) {
+    case 'player':
+      playerWins += 1;
+      break;
+    case 'computer':
+      computerWins += 1;
+      break;
   }
 
-  prompt('Play again? (y/n)');
-  let playAgain = readline.question();
-  while (playAgain !== 'y' && playAgain !== 'n') {
-    prompt('Enter y or n');
-    playAgain = readline.question();
-  }
-
-  if (playAgain === 'n') break;
+  displayWinner(choice, computerChoice, winner);
 }
 
+grandMaster(playerWins);
 
