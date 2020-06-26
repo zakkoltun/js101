e2fa79a4bf0cca0c7aa8e657f3530840f89bc451
Zak Koltun <zakkoltun@gmail.com>
2020-06-26 14:56:46 -0700

    Rock Paper Scissors add ons

--- lesson-2/rock-paper-scissors.js
+++ lesson-2/rock-paper-scissors.js
@@ -15,9 +15,6 @@ const RESULT_RESPONSES = {
   tie: "It's a tie"
 };
 
-let playerWins = 0;
-let computerWins = 0;
-
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -33,7 +30,9 @@ function pickWinner(choice, computerChoice) {
   }
 }
 
-function displayWinner(choice, computerChoice, winner) {
+function displayWinner(choice, computerChoice, playerWins, computerWins) {
+  let winner = pickWinner(choice, computerChoice);
+
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
   prompt(RESULT_RESPONSES[winner]);
   prompt(`You have ${playerWins} wins, computer has ${computerWins}`);
@@ -48,15 +47,70 @@ function grandMaster(playerWins) {
 }
 
 
+function startsWith(string, startingChars) {
+  let regex = new RegExp('^' + startingChars, 'g');
+  return regex.test(string);
+}
+
+// Gets the shortest unique abbreviation of each word in list
+// Ex. abbreviations(['rock', 'paper', 'scissors', 'spock'])
+//   => ['r', 'p', 'sc', 'sp']
+function abbreviations(words) {
+  let startingStrings = [];
+
+  words.forEach((word, idx1) => {
+    for (let i = 1; i <= word.length; i += 1) {
+      let lettersToCheck = word.slice(0, i);
+
+      let wordsWithSameStart = words.filter((otherWord, idx2) => {
+        return (idx1 !== idx2 && startsWith(otherWord, lettersToCheck));
+      });
+
+      if (wordsWithSameStart.length === 0) {
+        startingStrings.push(lettersToCheck);
+        break;
+      }
+    }
+  });
+
+  return startingStrings;
+}
+
+// Maps a list of abbreviations to the corresponding words
+// Ex: ['sc', 'sp], ['scissors', 'spock'] => { sc: 'scissors', sp: 'spock' }
+function mapAbbreviationsToWords(abbreviations, words) {
+  let abbreviationsToWords = {};
+  abbreviations.forEach(abbreviation => {
+    let matchingWords = words.filter(word => startsWith(word, abbreviation));
+    if (matchingWords.length === 1) {
+      abbreviationsToWords[abbreviation] = matchingWords[0];
+    }
+  });
+
+  return abbreviationsToWords;
+}
+
+let choiceAbbreviations = abbreviations(VALID_CHOICES);
+let abbreviationsToWords =
+  mapAbbreviationsToWords(choiceAbbreviations, VALID_CHOICES);
+
+let playerWins = 0;
+let computerWins = 0;
+
 while (playerWins < 5 && computerWins < 5) {
   prompt('----------------------');
   prompt('First to 5 wins is the grand master!\n');
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
   let choice = readline.question();
 
-  while (!VALID_CHOICES.includes(choice)) {
+  while (!VALID_CHOICES.includes(choice) &&
+          !choiceAbbreviations.includes(choice)) {
     prompt("That's not a valid choice");
     choice = readline.question();
+  }
+
+  if (!VALID_CHOICES.includes(choice) && choiceAbbreviations.includes(choice)) {
+    choice = abbreviationsToWords[choice];
   }
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
@@ -72,7 +126,7 @@ while (playerWins < 5 && computerWins < 5) {
       break;
   }
 
-  displayWinner(choice, computerChoice, winner);
+  displayWinner(choice, computerChoice, playerWins, computerWins);
 }
 
 grandMaster(playerWins);
